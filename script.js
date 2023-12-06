ac71e1c66efb721fb038fb7d518a74a80577486e
Hussien-Talha <hussien.a.m.talha@gmail.com>
2023-12-06 19:00:59 +0200

    testing...

--- script.js
+++ script.js
@@ -140,38 +140,42 @@ let touchStartY;
 canvas.addEventListener("touchstart", handleTouchStart);
 canvas.addEventListener("touchmove", handleTouchMove);
 
+// ... (existing code)
+
 // Define functions to handle touch events
 function handleTouchStart(event) {
-  touchStartX = event.touches[0].clientX;
-  touchStartY = event.touches[0].clientY;
-}
-
-function handleTouchMove(event) {
-  if (!touchStartX || !touchStartY) {
-    return;
+    touchStartX = event.touches[0].clientX;
+    touchStartY = event.touches[0].clientY;
   }
-
-  let touchEndX = event.touches[0].clientX;
-  let touchEndY = event.touches[0].clientY;
-
-  let deltaX = touchEndX - touchStartX;
-  let deltaY = touchEndY - touchStartY;
-
-  // Determine the swipe direction
-  if (Math.abs(deltaX) > Math.abs(deltaY)) {
-    // Horizontal swipe
-    snakeDX = deltaX > 0 ? gridSize : -gridSize;
-    snakeDY = 0;
-  } else {
-    // Vertical swipe
-    snakeDY = deltaY > 0 ? gridSize : -gridSize;
-    snakeDX = 0;
-  }
-
-  // Reset touch coordinates
-  touchStartX = null;
-  touchStartY = null;
-}
+  
+  function handleTouchMove(event) {
+    event.preventDefault(); // Prevent the default behavior
+  
+    if (!touchStartX || !touchStartY) {
+      return;
+    }
+  
+    let touchEndX = event.touches[0].clientX;
+    let touchEndY = event.touches[0].clientY;
+  
+    let deltaX = touchEndX - touchStartX;
+    let deltaY = touchEndY - touchStartY;
+  
+    // Determine the swipe direction
+    if (Math.abs(deltaX) > Math.abs(deltaY)) {
+      // Horizontal swipe
+      snakeDX = deltaX > 0 ? gridSize : -gridSize;
+      snakeDY = 0;
+    } else {
+      // Vertical swipe
+      snakeDY = deltaY > 0 ? gridSize : -gridSize;
+      snakeDX = 0;
+    }
+  
+    // Reset touch coordinates
+    touchStartX = null;
+    touchStartY = null;
+  }  
 
 // Add an event listener for the keydown event
 document.addEventListener("keydown", handleInput);
